2d9effdb4257c3834e6e4110e4a6e1dc8f05987e
Jeniffer souza <ajenifferbsouza@gmail.com>
2023-06-17 11:15:01 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using SenaiApp.Entidades;
+using SenaiBanco.Contexto;
+using SenaiBanco.Entidades;
+
+namespace SenaiApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var contexto = new SenaiContext();
+            var escola = contexto.Escola.Where(c => c.Nome == "Senai").FirstOrDefault();
+
+
+            //var escola = new Escola()
+            //{
+            //    Nome = "Bom pastor",
+            //   Endereco = new Endereco()
+            //   {
+            //        Rua = "AV Nereu Ramod",
+            //        Bairro = "Centro",
+            //        Estado = "SC",
+            //        Cidade = "Chapecó"
+            //    }
+            //};
+
+            //contexto.Escola.Add(escola);
+            //contexto.SaveChanges();
+
+            var professor = new Professor()
+            {
+                Nome = "Keni Borges",
+                EscolaId = escola.Id
+            };
+            
+
+        }
+    }
+}
